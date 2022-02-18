7789a7501d9e3e035df30a5531c21a4143c69beb
FabioVianna10 <fbo_vianna@hotmail.com>
2022-02-17 22:14:52 -0300

    atualizando

---
+++ script.js
@@ -0,0 +1,20 @@
+let name= "Fábio";
+let age= 29;
+let stars = 10.10;
+let isSubscribed = true;
+
+
+let student = {
+    name:"Fábio",
+    age:29,
+    weigth:77.4,
+    isSubscribed: true,}
+
+    
+let novo = []
+    
+ novo = [
+    student
+]
+
+console.log(novo);
